8cb098a7c68f40ba0deee1630f50d49ab28bfc5c
1Lucifer1 <2281854129@qq.com>
2021-12-19 22:03:46 +0800

    fix: 改一个bug

--- FileParserServer/JsonStorage.py
+++ FileParserServer/JsonStorage.py
@@ -7,11 +7,11 @@ class JsonExporter:
     def export_json(json_path, project_list, node_list, relation_list):
         json_list = {"project": [], "entity_list": [], "relation_list": []}
         for project in project_list:
-            json_list[0]["project"].append(project.to_dic())
+            json_list["project"].append(project.to_dic())
         for node in node_list:
-            json_list[0]["entity_list"].append(node.to_dic())
+            json_list["entity_list"].append(node.to_dic())
         for relation in relation_list:
-            json_list[0]["relation_list"].append(relation.to_dic())
+            json_list["relation_list"].append(relation.to_dic())
         # export_str = json.dumps(json_list)
         file_path = json_path
         with open(file_path, 'w', encoding="utf-8") as f:
